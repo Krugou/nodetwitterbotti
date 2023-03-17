513614227e48bd62bbf19c8f47a00a2bf5421721
Krugou <59641229+Krugou@users.noreply.github.com>
2023-03-17 08:25:00 +0200

    asd

--- timekeeper/jak.js
+++ timekeeper/jak.js
@@ -8,87 +8,100 @@ const channelID = '1084944685568638976';
 
 jakbot.login(process.env.DISCORD_TOKEN);
 jakbot.once(Events.ClientReady, c => {
-    // console.log(`Ready! Logged in as ${c.user.tag}`);
+  // console.log(`Ready! Logged in as ${c.user.tag}`);
 
 });
 
 
 jakbot.on('ready', jakbot => {
-    const channel = jakbot.channels.cache.get(channelID);
-    channel.messages.fetch({ limit: 10 })
-        .then(messages => {
-            // Delete the messages
-            channel.bulkDelete(messages);
-        })
-        .catch(console.error);
-    jakbot.user.setUsername('TimeKeeper');
-    jakbot.user.setActivity('Nodejs', { type: 'PLAYING' });
-    // if today is monday
-    const today = new Date();
-    const day = today.getDay();
-    if (day === 1) {
-        jakbot.channels.cache.get(channelID).
-            send('Hello, I am the TimeKeeper, I will keep track of current time and remind you when we have a classes coming up! have a nice week!');
-    }
+  const channel = jakbot.channels.cache.get(channelID);
+  channel.messages.fetch({ limit: 100 })
+    .then(messages => {
+      // Delete the messages
+      channel.bulkDelete(messages);
+    })
+    .catch(console.error);
+  jakbot.user.setUsername('TimeKeeper');
+  jakbot.user.setActivity('Nodejs', { type: 'PLAYING' });
+  // if today is monday
+  const today = new Date();
+  const day = today.getDay();
+  if (day === 1) {
+    jakbot.channels.cache.get(channelID).
+      send('Hello, I am the TimeKeeper, I will keep track of current time and remind you when we have a classes coming up! have a nice week!');
+  }
 });
 
 const checkReservations = () => {
-    jakbot.on('ready', jakbot => {
+  jakbot.on('ready', jakbot => {
 
 
-        fetch('https://opendata.metropolia.fi/r1/reservation/search', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Basic ' + btoa(process.env.APIKEYMETROPOLIA),
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "studentGroup": ["tvt21-m"]
-            })
-        })
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                for (let i = 0; i < data.reservations.length; i++) {
-                    const dateStr = data.reservations[i].startDate;
-                    const date = new Date(dateStr);
+    fetch('https://opendata.metropolia.fi/r1/reservation/search', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa(process.env.APIKEYMETROPOLIA),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "studentGroup": ["tvt21-m"]
+      })
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const today = new Date();
+        jakbot.channels.cache.get(channelID).send('------------------------------------------------------------------');
 
-                    // check if the reservation is between today and five days from now
-                    const today = new Date();
-                    const fiveDaysFromNow = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
-                    const isBetweenTodayAndFiveDaysFromNow = date >= today && date <= fiveDaysFromNow;
+        jakbot.channels.cache.get(channelID).send('Date Today: ' + today.getDate.toString());
 
-                    // loop if StartDate is today for reservation or tomorrow for reservation
-                    if (isBetweenTodayAndFiveDaysFromNow) {
-                        const endDate = data.reservations[i].endDate;
-                        const endTime = endDate.substring(11, 16);
-                        // get time from startDate
-                        const time = dateStr.substring(11, 16);
-                        // get time from endDate
-                        // get date from startDate
-                        const date = dateStr.substring(0, 10);
-                        jakbot.channels.cache.get(channelID).send(date + ' Aiheena on: ' + data.reservations[i].subject + ' aika välillä: ' + time + ' - ' + endTime);
+        for (let i = 0; i < data.reservations.length; i++) {
+          const dateStr = data.reservations[i].startDate;
+          const date = new Date(dateStr);
 
-                        // if reservations has more than 2 resources
-                        if (data.reservations[i].resources.length > 2) {
-                            jakbot.channels.cache.get(channelID).send(' joka on luokassa: ' + data.reservations[i].resources[2].code);
+          // check if the reservation is between today and five days from now
+          const fiveDaysFromNow = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
+          const isBetweenTodayAndFiveDaysFromNow = date >= today && date <= fiveDaysFromNow;
 
-                        } else {
-                            jakbot.channels.cache.get(channelID).send('Luultavasti on etäopetusta tai ei ole luokkaa');
-                        }
-                    }
-                }
-
-
+          // loop if StartDate is today for reservation or tomorrow for reservation
+          if (isBetweenTodayAndFiveDaysFromNow) {
+            const endDate = data.reservations[i].endDate;
+            const endTime = endDate.substring(11, 16);
+            // get time from startDate
+            const time = dateStr.substring(11, 16);
+            // get time from endDate
+            // get date from startDate
+            const date = dateStr.substring(0, 10);
+            const subjectsCount = data.reservations.length; // Get the total count of subjects in the response
+            const currentSubjectIndex = i; // Get the index of the current subject in the response
+            jakbot.channels.cache.get(channelID).send('Reservation ID: ' + data.reservations[i].id);
+            jakbot.channels.cache.get(channelID).send('Date: ' + date);
+            jakbot.channels.cache.get(channelID).send('Subject: ' + data.reservations[i].subject);
+            jakbot.channels.cache.get(channelID).send('Start Time: ' + time);
+            jakbot.channels.cache.get(channelID).send('End Time: ' + endTime);
+            jakbot.channels.cache.get(channelID).send(`This is subject ${currentSubjectIndex}/${subjectsCount}.`);
+            const resources = data.reservations[i].resources;
+            const room = resources.find(resource => resource.type === 'room');
+            if (room) {
+              const roomName = room.code;
+              jakbot.channels.cache.get(channelID).send(`The reservation is for room: ${roomName}`);
+            } else {
+              jakbot.channels.cache.get(channelID).send('The reservation is for remote teaching.');
             }
-            )
-            .catch(err => {
-                console.log(err);
-            });
+            jakbot.channels.cache.get(channelID).send('------------------------------------------------------------------');
+
+          }
+        }
 
 
-    });
+      }
+      )
+      .catch(err => {
+        console.log(err);
+      });
+
+
+  });
 };
 
 checkReservations();
