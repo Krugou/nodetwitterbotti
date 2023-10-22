4a595204ee16620ca5765220e4851514349565a9
Krugou <59641229+Krugou@users.noreply.github.com>
2023-10-22 16:23:30 +0300

    Update allofthem.js

--- timekeeper/allofthem.js
+++ timekeeper/allofthem.js
@@ -1,4 +1,4 @@
-import {Client, Events, GatewayIntentBits} from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import 'dotenv/config';
 import fetch from 'node-fetch';
 
