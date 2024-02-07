69d0189eadf0623ba7d90d2d5a4607f26803d60b
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-07 07:26:05 +0200

    Update user authentication logic

---
+++ timekeeper/ecosystem.config.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+	apps: [
+		{
+			name: 'weather',
+			script: 'weather.js',
+			watch: true,
+			cron_restart: '0 5,8,12 * * 1-5',
+		},
+		{
+			name: 'jak',
+			script: 'jak.js',
+			watch: true,
+			cron_restart: '0 4 * * 1-5',
+		},
+		{
+			name: 'lunch',
+			script: 'lunch.js',
+			watch: true,
+			cron_restart: '0 6 * * 1-5',
+		},
+		{
+			name: 'onlinechecker',
+			script: 'onlinechecker.js',
+			watch: true,
+			cron_restart: '0 */3 * * *',
+		},
+		{
+			name: 'imagesaver',
+			script: 'imagesaver.js',
+			watch: true,
+		},
+	],
+};
