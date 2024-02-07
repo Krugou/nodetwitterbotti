692e347baba02ba5a6e84225cb706ed05e42628a
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-07 07:37:25 +0200

    Update project directory and dependencies

--- update-script.sh
+++ update-script.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
 # Change to the project directory
-cd /home/aanderson/LuxDei
+cd /home/aleksino/nodetwitterbotti
 
-# Start the SSH agent and add the SSH key
-eval `ssh-agent -s`
-ssh-add /home/aanderson/.ssh/id_ed25519
 
 # Perform a Git pull to update the code
 if git pull | grep -q 'Already up to date.'; then
   echo 'No changes pulled.'
 else
-  # If changes were pulled, navigate to the frontend directory
-  cd frontend
-  npm install
-  npm run build
-  cd ..
-  cd backend
+ 
+  npm i
+  cd timekeeper
   npm i
 fi
