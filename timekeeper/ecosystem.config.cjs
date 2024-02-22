fd14d20dac52ddb8324f3fc11f6a5573bb0f8cb7
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-22 21:11:17 +0200

    Add gitPuller script to ecosystem.config.cjs

--- timekeeper/ecosystem.config.cjs
+++ timekeeper/ecosystem.config.cjs
@@ -45,5 +45,14 @@ module.exports = {
 			ignore_watch: ['node_modules', 'logs', 'package-lock.json'],
 			watch_delay: 3000,
 		},
+		{
+			name: 'gitPuller'
+			script: 'gitPuller.js',
+			watch: true,
+			cron_restart: '0 1 * * *',
+			min_uptime: 10000,
+			ignore_watch: [ 'node_modules', 'logs', 'package-lock.json' ],
+			watch_delay: 3000,
+		}
 	],
 };
